aefb656607f438e81aaf85f41fe7bccd6fdc242d
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2017-10-23 20:08:47 -0400

    Use only 2-cores in Travis multi-core tests

--- tools/tests/Travis-MOM6/run_tests.sh
+++ tools/tests/Travis-MOM6/run_tests.sh
@@ -6,7 +6,7 @@ function run_test {
  echo ; echo Running $3 with $2 core using $1 ...
  cd MOM6-examples/ocean_only/$3
  mkdir -p RESTART
- mpirun -n $2 $here/build/$1/MOM6 1> log 2> errlog
+ time mpirun -n $2 $here/build/$1/MOM6 1> log 2> errlog
  if [ $? == 0 ]; then \
   echo "    ..." $3 completed successfully.
  else
@@ -32,13 +32,13 @@ cat stats.md5
 
 # Multi-core tests with symmetric executable
 run_test symocn 1 unit_tests
-run_test symocn 4 double_gyre
+run_test symocn 2 double_gyre
 run_test symocn 2 flow_downslope/layer
 run_test symocn 2 flow_downslope/rho
 run_test symocn 2 flow_downslope/sigma
 run_test symocn 2 flow_downslope/z
-run_test symocn 4 benchmark
-run_test symocn 4 circle_obcs
+run_test symocn 2 benchmark
+run_test symocn 2 circle_obcs
 
 # Check that checksums match
 md5sum -c stats.md5
